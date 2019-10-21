c0f5b6c62bc36eba7051a89449b61d08b671e9c3
Issa Gorissen <issa-gorissen@usa.net>
2019-10-21 13:45:40 +0200

    Fixing bug which crashed the app when unknow number was calling in

--- app/src/main/java/lu/binarylogic/notificationforwarder/MissedCallReceiver.java
+++ app/src/main/java/lu/binarylogic/notificationforwarder/MissedCallReceiver.java
@@ -19,14 +19,26 @@ public class MissedCallReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(intent.getAction())
                 && TelephonyManager.EXTRA_STATE_RINGING.equals(intent.getStringExtra(TelephonyManager.EXTRA_STATE))) {
-            Log.v(TAG, intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+
 
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
             StrictMode.setThreadPolicy(policy);
 
-            String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            String contactName = ContactLookup.lookupContactNameFromNumber(context.getContentResolver(), number);
-            String formattedNumber = ContactLookup.formattedPhoneNumber(number, context);
+            String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            String contactName = "Unknown";
+            String formattedNumber = "Private phone number";
+
+            if (phoneNumber == null) {
+                Log.v(TAG, "Calling phone number is unknown");
+
+            } else {
+                Log.v(TAG, phoneNumber);
+
+                String number = phoneNumber;
+                contactName = ContactLookup.lookupContactNameFromNumber(context.getContentResolver(), number);
+                formattedNumber = ContactLookup.formattedPhoneNumber(number, context);
+            }
+
 
             Log.v(TAG, contactName);
             Log.v(TAG, formattedNumber);
